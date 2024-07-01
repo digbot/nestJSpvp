869feca97aff459f8da5c52003108f169410fe98
Dimitar Sashev Dimitrov <dimitar.sashev@gmail.com>
2024-07-01 11:37:01 +0300

    ok

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -3,11 +3,11 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 const config = {
   type: 'mysql',
-  host: process.env.DB_HOST,
+  host: 'localhost',
   port: parseInt(process.env.DB_PORT) || 3306,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  username: 'pvp',
+  password: 'password',
+  database: 'pvp',
   synchronize: true,
   entities: ['src/typeorm/entities/**/*.entity{.ts,.js}'],
   //migrations: ['src/migrations/*{.ts,.js}'],
