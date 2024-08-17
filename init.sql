c58745ceeb64bfdfaa6f0a1c5536b016a12b7fa7
Dimitar Sashev Dimitrov <dimitar.sashev@gmail.com>
2024-08-17 11:23:23 +0300

    update_docker_db

--- init.sql
+++ init.sql
@@ -10,3 +10,14 @@ CREATE TABLE  IF NOT EXISTS   `month` (
   `invest` int NOT NULL,
   PRIMARY KEY (`id`)
 );
+
+
+CREATE TABLE  IF NOT EXISTS  `month_save2` (
+  `in` int NOT NULL,
+  `out` int NOT NULL,
+  `date` datetime NOT NULL,
+  `buffer` int DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `invest` int NOT NULL,
+  PRIMARY KEY (`id`)
+);
